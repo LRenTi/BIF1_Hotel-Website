defbe39a234cae77035013bb368dc565148eeab3
Lorenz <LorenzRenti@outlook.com>
2023-11-17 17:49:34 +0100

    Update: Navbar

--- index.php
+++ index.php
@@ -4,34 +4,53 @@
         <meta charset="UTF-8">
         <link href="stylesheet.css" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     </head>
     <header>
         <div class="bc-blue">
-            <!--Navbar-leiste-->
-            <nav class="d-flex container">
+            <!--Header leiste-->
+            <div class="d-flex container-md">
                 <!-- Logo -->
-                <div class="d-flex pt-3 pb-3 justify-content-start">
-                    <img class="img-fluid" alt="Logo" src="content\logo\LogoHotel.svg" width="100">
-                    <h5 class="font-weight-bold text-white d-flex align-self-center" style="font-size:50px;">Hotel</h5>
+                <a class="td-none" href="index.php">
+                    <div class="d-flex pt-3 pb-3 justify-content-start">
+                        <img class="img-fluid" alt="Logo" src="content\logo\LogoHotel.svg" width="100">
+                        <h5 class="font-weight-bold text-white d-flex align-self-center" style="font-size:50px;">Hotel</h5>
+                    </div>
+                </a>
+                <!--Upper Start-->
+                <div class="navbar d-flex flex-grow-1 justify-content-end align-items-end mb-3 d-none d-sm-flex">
+                    <a type="button" class="btn btn-outline me-2" href="index.php?register=true">Register</a>                        
+                    <a type="button" class="btn btn-outline me-2" data-bs-toggle="modal" data-bs-target="#Login">Login</a>
+                    <a type="button" class="btn btn-gold" href="">Jetzt Buchen!</a>
+                    </ul>
                 </div>
-                <!--Menu Start-->
-                <div class="d-flex flex-grow-1 justify-content-end align-items-end mb-3">
-                    <ul class="nav col-12 col-lg-auto ms-lg-auto mb-2 ms-2 mr-3">
+                <!--Upper Ends-->
+            </div>
+        </div>
+        <!-- Nav menu -->
+        <nav class="navbar navbar-expand-sm navbar-light bg-lignt">
+            <div class="container">
+            <div class="d-flex d-sm-none justify-content-center">
+                        <a type="button" class="btn btn-gold cblue" href="">Jetzt Buchen!</a>       
+                    </div>
+                <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#n_bar" aria-controls="navbarNavAltMarkup" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="n_bar">
+                    <div class="d-flex justify-content-start d-sm-none mt-2 mb-2">
+                        <a type="button" class="btn btn-outline cblue me-2" data-bs-toggle="modal" data-bs-target="#Login">Login</a>
+                        <a type="button" class="btn btn-outline cblue me-2" href="index.php?register=true">Register</a>  
+                    </div>
+                    <ul class="navbar-nav">
                         <li><a href="index.php" class="nav-point px-2">Home</a></li>
                         <li><a href="index.php?faqs=true" class="nav-point px-2">FAQs</a></li>
                         <li><a href="index.php?impressum=true" class="nav-point px-2 ">Impressum</a></li>
                     </ul>
-                    <!--Menu Ends-->
-                    <!--Login/Register Start-->
-                    <div class="text-end mb-2 d-none d-lg-block">
-                       <a href="index.php?register=true" type="button" class="btn btn-outline">Register</a>                           
-                       <a type="button" class="btn btn-white" data-bs-toggle="modal" data-bs-target="#Login">Login</a>
-                    </div>
                 </div>
-                <!--Login/Register Ends-->
-            </nav>
-        </div>
+            </div>
+        </nav>
+        <hr class="solid m-0 p-0">
+        
+
         <!-- Modal Start -->
         <div class="modal fade" id="Login" tabindex="-1" aria-labelledby="Loginlabel" aria-hidden="true">
             <div class="modal-dialog">
@@ -77,6 +96,9 @@
             }
         ?>
     </main>
+    <body>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    </body>
     <footer class="bc-blue text-white text-center mb-0">
         <p>&copy; 2023 L&A Hotel. Alle Rechte vorbehalten.</p>
     </footer>
--- pages/home.php
+++ pages/home.php
@@ -3,26 +3,7 @@
     <head>
         <title>Willkommen im L&A Hotel!</title>
     </head>
-    <header class="masthead">
-  <div class="container h-100">
-    <div class="row h-100 align-items-center">
-      <div class="col-12 text-center">
-        <h1 class="fw-light">Vertically Centered Masthead Content</h1>
-        <p class="lead">A great starter layout for a landing page</p>
-      </div>
-    </div>
-  </div>
-</header>
-    <body>
-        <div class="back_img1">
-            <div class="container">
-                <div class="row h-100 align-items-center">
-                    <div class="col-12 text-center">
-                        <h1 class="fw-light">Vertically Centered Masthead Content</h1>
-                        <p class="lead">A great starter layout for a landing page</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </body>
+<body>
+  <img class="" src="content\pictures\StockHotel.jpg" width="100%">
+</body>
 </html>

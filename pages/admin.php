11c17eac453a85c60f66b5265320d7da2d1e67ad
Lorenz <LorenzRenti@outlook.com>
2023-11-26 17:07:59 +0100

    File Upload: Resized func

--- pages/admin.php
+++ pages/admin.php
@@ -22,6 +22,8 @@
             </div>
             
             <?php
+
+
             /* Upload picture */
             if(isset($_FILES["picture"]))
             {
@@ -32,27 +34,46 @@
                     move_uploaded_file($_FILES["picture"]["tmp_name"], $destination);
                     
                     echo("<p> Bild wurde hochgeladen! <p>");
+                    
+                    // Resize image
+                    $resizedDestination = getcwd(). "/uploads/thumbnails/" . "resized_" .uniqid() . "_" . $_FILES["picture"]["name"];
+                    $sourceImage = imagecreatefromjpeg($destination);
+                    $resizedImage = imagecreatetruecolor(720, 480);
+                    imagecopyresized($resizedImage, $sourceImage, 0, 0, 0, 0, 720, 480, imagesx($sourceImage), imagesy($sourceImage));
+                    imagejpeg($resizedImage, $resizedDestination);
+                    imagedestroy($sourceImage);
+                    imagedestroy($resizedImage);
+                    
+                    echo("<p> Bild wurde erfolgreich auf 720x480 Pixel umgeändert! <p>");
                 } 
                 else
                 {
                     echo "Der Dateityp wird nicht unterstützt.";
                 }
             }
+
             ?>
 
             <?php
+            echo("<div class=\"row\">");
+                echo("<div class=\"col col-6\">");
             /* Alle Picture anzeigen im Upload Folder */
             $files = scandir("uploads");
 
+            echo "<h2 class=\"mt-2 fw-bold\" >Pictures</h2>";
+
             if ($files !== false) {
                 if (count($files) > 2){
-                    echo "<h2 class=\"mt-2\" >Alle Uploads</h2>";
+                    echo "<h3 class=\"mt-2\" >Alle Uploads</h3>";
                 }
-                else { echo "<h5 class=\"mt-2\">Keine Uploads vorhande!</h5>";}
+                else { echo "<h5 class=\"mt-2\">Keine Uploads vorhanden!</h5>";}
 
-                for ($i = 2; $i < count($files); $i++) {
+                for ($i = 0; $i < count($files); $i++) {
                     $file = $files[$i];
-                    if (pathinfo($file, PATHINFO_EXTENSION) == "jpg" || pathinfo($file, PATHINFO_EXTENSION) == "jpeg") {
+                    if ($file == "." || $file == ".." || $file == "thumbnails") {
+                        continue;
+                    }
+                    else if (pathinfo($file, PATHINFO_EXTENSION) == "jpg" || pathinfo($file, PATHINFO_EXTENSION) == "jpeg") {
                         echo '<a href="index.php?include=admin&pic='. $file . '">' . $file . '</a><br>';
                     } else {
                         echo $file . "<br>";
@@ -73,7 +94,48 @@
                     echo('</div>');
                 }
             }
+                echo("</div>");
+                echo("<div class=\"col col-6\">");
+                
+            /* Alle Thumbnails anzeigen im Upload Folder */
+            $filesT = scandir("uploads/thumbnails");
+
+            echo "<h2 class=\"mt-2 fw-bold\" >Thumbnails</h2>";
+
+            if ($filesT !== false) {
+                if (count($filesT) > 2){
+                    echo "<h3 class=\"mt-2\" >Alle Thumbnails</h3>";
+                }
+                else { echo "<h5 class=\"mt-2\">Keine Thumbnails vorhanden!</h5>";}
+
+                for ($i = 0; $i < count($filesT); $i++) {
+                    $fileT = $filesT[$i];
+                    if ($fileT == "." || $fileT == ".." || $fileT == "thumbnails") {
+                        continue;
+                    }
+                    else if (pathinfo($fileT, PATHINFO_EXTENSION) == "jpg" || pathinfo($fileT, PATHINFO_EXTENSION) == "jpeg") {
+                        echo '<a href="index.php?include=admin&tpic='. $fileT . '">' . $fileT . '</a><br>';
+                    } else {
+                        echo $fileT . "<br>";
+                    }
+                }
+            } else {
+                echo "ERROR: Failed to open directory.";
+            }
+
+            if(isset($_GET["tpic"]))
+            {
+
+                if (in_array($_GET["tpic"], $filesT))
+                {
+                    echo('<div class="mt-3 ms-3">');
+                    echo('<h3>Selected Thumbnail (resized)</h3>');
+                    echo '<img src="uploads/thumbnails/' . $_GET["tpic"] . '" alt="Bild" width="auto" height="200" class="rounded border mb-3">';
+                    echo('</div>');
+                }
+            }
+            echo("</div>");
             ?>
-        </div>
-    </body>
-</html>
+            </div>
+            </body>
+            </html>

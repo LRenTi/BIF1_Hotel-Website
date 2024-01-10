fc334a2837bc16f8860928b04f801ed0f59121df
Lorenz <LorenzRenti@outlook.com>
2024-01-11 00:56:30 +0100

    Footer activated

--- pages/footer.php
+++ pages/footer.php
@@ -1,5 +1,3 @@
-<!--
-    <footer class="bc-blue text-white mb-0 navbar navbar-fixed-bottom">
-        <p>&copy; 2023 L&A Hotel. Alle Rechte vorbehalten.</p>
+    <footer class="bc-blue text-white mb-0 navbar navbar-fixed-bottom d-flex justify-content-center">
+        <p class="pt-2">&copy; 2023 L&A Hotel. Alle Rechte vorbehalten.</p>
     </footer>
--->
